d47963a414f76895eb96f8f8de2f57ad41a9c6fe
FayezRizqi <rizkipro122@gmail.com>
2023-04-19 15:20:43 +0700

    tambahan f/led2

--- GG-Myskill2/projek.ino
+++ GG-Myskill2/projek.ino
@@ -1,10 +1,16 @@
+const int ledPin2 = 6;
+
 void setup (){
+    pinMode (ledPin2, OUTPUT);
+    
 
 
 
 }
 
 void loop (){
+    digitalWrite (ledPin2, HIGH);// jika button ditekan maka lampu hidup
+    digitalWrite (ledPin2, LOW);//jika button tidak ditekan maka lampu mati
 
 
     
