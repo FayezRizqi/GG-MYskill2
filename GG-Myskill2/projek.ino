a85098db3d97c055f92ca68c820627ca3c73a58e
muhammadrshihab22 <muhammadrshihab22@gmail.com>
2023-04-19 15:51:18 +0700

    tambahan led1

--- GG-Myskill2/projek.ino
+++ GG-Myskill2/projek.ino
@@ -1,7 +1,13 @@
-void setup()
-{ // test
+const int ledPin1 = 6
+
+    void
+    setup()
+{
+    pinMode(ledPin1, OUTPUT);
 }
 
 void loop()
 {
+    digitalWrite(ledPin1, HIGH);
+    digitalWrite(ledPin1, LOW);
 }
